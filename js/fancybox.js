eccc124acb20e5811fc66ee9ef9555899a5e2048
Razin Ahmed <raheenahmee@gmail.com>
2024-12-13 15:16:49 +0500

    resume update

--- js/fancybox.js
+++ js/fancybox.js
@@ -1,16 +1,16 @@
-// / Fancybox Config
-$('data-fancybox="gallery"').fancybox({
-  buttons: [
-    "slideShow",
-    "thumbs",
-    "zoom",
-    "fullScreen",
-    "share",
-    "close"
-  ],
-  loop: false,
-  protect: true
-});
+// // / Fancybox Config
+// $('data-fancybox="gallery"').fancybox({
+//   buttons: [
+//     "slideShow",
+//     "thumbs",
+//     "zoom",
+//     "fullScreen",
+//     "share",
+//     "close"
+//   ],
+//   loop: false,
+//   protect: true
+// });
 
 
 
